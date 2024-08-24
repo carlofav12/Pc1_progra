5d1f6f4cfc2a41061f2efc9658472ac72463fca6
carlofav12 <carlofav12@gmail.com>
2024-08-24 13:11:58 -0500

    operacion montos

--- Models/Operaciones.cs
+++ Models/Operaciones.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace practica1.Models
@@ -13,5 +14,34 @@ namespace practica1.Models
         public DateTime FechaOperacion { get; set; }
         public List<string>? Operacion { get; set; }
         public decimal MontoAbono { get; set; }
+        public decimal TotalPag { get; private set; }
+
+
+        public void CalcularInversion()
+        {
+            decimal totPag = 0;
+            decimal igv = 0.18m;
+
+            foreach (var instrumento in Operacion ?? new List<string>())
+            {
+                decimal monto = instrumento switch
+                {
+                    "S&P 500" => 500,
+                    "Dow jones" => 300,
+                    "Bonos US" => 120,
+                    _ => 0
+                };
+
+                decimal igvAp = monto * igv;
+                totPag += monto + igvAp;
+            }
+
+            //comision
+            decimal comision = MontoAbono <= 300 ? 1 : 3;
+            totPag += comision;
+
+            TotalPag = totPag;
+        }
+
     }
 }
